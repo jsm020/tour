4bdf4847b09a0d970daddd7aa984364b0ed2e886
Javoxir <78523543+jsm020@users.noreply.github.com>
2023-06-30 15:21:34 +0500

    izm

--- js/script.js
+++ js/script.js
@@ -15,29 +15,29 @@ window.onscroll = () => {
     loginForm.classList.remove('active');
 }
 
-menu.addEventListener('click' , () =>{
+menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 });
- 
 
-searchBtn.addEventListener('click' , () =>{
+
+searchBtn.addEventListener('click', () => {
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
 });
 
 
 
-formBtn.addEventListener('click' , () =>{
+formBtn.addEventListener('click', () => {
     loginForm.classList.add('active');
 });
 
-formClose.addEventListener('click' , () =>{
+formClose.addEventListener('click', () => {
     loginForm.classList.remove('active');
 });
 
 videoBtn.forEach(btn => {
-    btn.addEventListener('click', ()=>{
+    btn.addEventListener('click', () => {
         document.querySelector('.controls .active').classList.remove('active');
         btn.classList.add('active');
         let src = btn.getAttribute('data-src');
@@ -46,21 +46,44 @@ videoBtn.forEach(btn => {
 });
 
 var swiper = new Swiper(".review-slider", {
-    spaceBetween:20,
-    loop:true,
+    spaceBetween: 20,
+    loop: true,
     autoplay: {
-        delay:2500,
-        disableOnInteraction:false,
+        delay: 2500,
+        disableOnInteraction: false,
     },
-    breakpoints : {
-        640:{
-            slidesPerView:1,
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
         },
-        768:{
-            slidesPerView:1,
+        768: {
+            slidesPerView: 2,
         },
-        1024:{
-            slidesPerView:3,
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
         },
     },
 });
